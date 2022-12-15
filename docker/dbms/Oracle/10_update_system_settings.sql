b6962a5659d981e4bd0582b212a18b79a3e9f89f
Sergi Philipsen <philipsen.sergi@gmail.com>
2022-12-15 09:22:39 +0100

    Update 10_update_system_settings.sql
    Try to stop the database with abort.

--- docker/dbms/Oracle/10_update_system_settings.sql
+++ docker/dbms/Oracle/10_update_system_settings.sql
@@ -2,7 +2,7 @@ alter system set processes=400 scope=spfile;
 alter system set sessions=640 scope=spfile;
 alter system set transactions=704 scope=spfile;
 
-shutdown immediate;
+shutdown abort;
 startup;
 
 exit
