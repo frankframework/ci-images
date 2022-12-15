a9af048b2948ec1d520ebcf7e041480d3a299b1c
Sergi Philipsen <philipsen.sergi@gmail.com>
2022-12-15 09:46:27 +0100

    Revert "Update 10_update_system_settings.sql"
    This reverts commit b6962a5659d981e4bd0582b212a18b79a3e9f89f.

--- docker/dbms/Oracle/10_update_system_settings.sql
+++ docker/dbms/Oracle/10_update_system_settings.sql
@@ -2,7 +2,7 @@ alter system set processes=400 scope=spfile;
 alter system set sessions=640 scope=spfile;
 alter system set transactions=704 scope=spfile;
 
-shutdown abort;
+shutdown immediate;
 startup;
 
 exit
