877401f6d63eea2a6b1aaf049eb69f59626feeff
Tim van der Leeuw <tim@wearefrank.nl>
2023-08-03 15:05:02 +0200

    Add scripts to rebuild or start all databases. Make all scripts executable.

--- docker/dbms/MongoDB/rebuild.sh
+++ docker/dbms/MongoDB/rebuild.sh
@@ -6,4 +6,4 @@ docker-compose down --volumes --remove-orphans
 docker image prune --force
 docker volume prune --force
 
-docker build -t iaf-test-db-mysql .
+docker build -t iaf-test-db-mongodb .
---
+++ docker/dbms/rebuild-all.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")" || exit 1
+
+for dir in */ ; do
+  echo "$(pwd)/$dir"
+  old_dir="$(pwd)"
+  "$(pwd)/$dir/rebuild.sh"
+  cd "$old_dir" || exit 1
+done
